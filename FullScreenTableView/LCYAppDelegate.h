ba27de21301d28525c35586a33b66fb852828a57
Krishna Kotecha <krishna.kotecha@gmail.com>
2012-02-09 22:44:32 +0000

    Initial Commit

---
+++ FullScreenTableView/LCYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LCYAppDelegate.h
+//  FullScreenTableView
+//
+//  Created by Krishna Kotecha on 09/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FullScreenTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FullScreenTableView
+//
+//  Created by Krishna Kotecha on 09/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LCYAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LCYAppDelegate class]));
+    }
+}
